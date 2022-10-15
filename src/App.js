9c300036660d0e181e135795d1b600c0140cb5ba
Mayank Rastogi <mayankrustey2002@gmail.com>
2022-10-15 11:22:43 -0700

    Added boilerplate

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <button class="btn">Button</button>
+<button class="btn btn-primary">Button</button>
+<button class="btn btn-secondary">Button</button>
+<button class="btn btn-accent">Button</button>
+<button class="btn btn-ghost">Button</button>
+<button class="btn btn-link">Button</button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
